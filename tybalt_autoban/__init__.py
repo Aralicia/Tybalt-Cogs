b89558efbc520ac029e0cf689d3f82de6d2d8394
Aralicia <aralicia.moran@gmail.com>
2021-07-05 19:10:01 +0200

    New Feature : Autoban

---
+++ tybalt_autoban/__init__.py
@@ -0,0 +1,5 @@
+from .tybalt_autoban import TybaltAutoban
+
+def setup(bot):
+    bot.add_cog(TybaltAutoban())
+
---
+++ tybalt_autoban/tybalt_autoban.py
@@ -0,0 +1,72 @@
+import discord
+from redbot.core import checks, commands
+from redbot.core import Config
+
+class TybaltAutoban(commands.Cog):
+    """TybaltAutoban."""
+
+    def __init__(self):
+        super().__init__()
+        self.config = Config.get_conf(self, identifier=1840351931)
+        default_guild = {
+                'ban_like': [],
+                'ban_regex': []
+        }
+        self.config.register_guild(**default_guild)
+
+    @commands.command(pass_context=True, no_pm=True, aliases=["autoban"])
+    @checks.has_permissions(ban_members=True)
+    async def autoban_add(self, ctx, *match):
+        """Add a term to the autoban filter
+        Example:
+        !autoban_add "Zythas"
+        """
+
+        msg = " ".join(match).lower()
+        matches = await self.config.guild(ctx.guild).ban_like()
+        if msg not in matches:
+            matches.append(msg)
+            await self.config.guild(ctx.guild).ban_like.set(matches)
+            await ctx.message.channel.send("Added to autoban : `{}`".format(msg))
+        else:
+            await ctx.message.channel.send("Already in autoban : `{}`".format(msg))
+
+    @commands.command(pass_context=True, no_pm=True)
+    @checks.has_permissions(ban_members=True)
+    async def autoban_del(self, ctx, *match):
+        """Removes a term to the autoban filter
+        Example:
+        !autoban_del "Zythas"
+        """
+
+        msg = " ".join(match)
+        matches = await self.config.guild(ctx.guild).ban_like()
+        if msg in matches:
+            matches.remove(msg)
+            await self.config.guild(ctx.guild).ban_like.set(matches)
+            await ctx.message.channel.send("Removed from autoban : `{}`".format(msg))
+        else:
+            await ctx.message.channel.send("Not in autoban : `{}`".format(msg))
+
+
+    @commands.command(pass_context=True, no_pm=True)
+    @checks.has_permissions(ban_members=True)
+    async def autoban_fix(self, ctx, *match):
+        old_matches = await self.config.guild(ctx.guild).ban_like()
+        new_matches = []
+        for match in old_matches:
+            new_matches.append(match.lower())
+        #await self.config.guild(ctx.guild).ban_like.set(new_matches)
+        
+
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        member_name = member.name.lower()
+        matches = await self.config.guild(member.guild).ban_like()
+        for match in matches:
+            if match in member_name:
+                print("Ban ! ({})".format(member.name))
+                await member.ban(reason="Autoban : {}".format(match))
+                break
+
